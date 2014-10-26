a58e3dee9d880a35d4011eb6454b95f53d4df495
Adam Michael <ajmichael0@gmail.com>
2014-10-26 01:17:02 -0400

    completed BM25 scoring algorithm

--- BM25Scoring/score.py
+++ BM25Scoring/score.py
@@ -1,22 +1,74 @@
-#!/usr/lib/env python
+#!/usr/bin/env python
 
 import sys
+import re
+import os
 
-def readIDF(path):
-     
+idfpath = 'data/idf.dat'
+tfpath = 'data/tfcount.dat'
+wordcountpath = 'data/wordcount.dat'
+inputdir = '../Presidents'
 
-def score(D,Q,tfmap,idfmap,k1,b):
-    total = 0
-    for qi in Q:
-        if qi not in tfmap[D].keys():
-            pass
-        tf = tfmap[D][qi]
-        idf = idfmap[qi]
-        total += idf * tf * (k1+1) / (tf + k1 * (1 - b + b * 
+
+splice = lambda x: x.strip().split('\t')
+
+def readWordCount(path):
+    wc = {}
+    with open (path,'r') as f:
+        for line in map(splice, f.readlines()):
+            wc[line[0]] = int(line[1])
+    return wc
         
 
+def readTF(path):
+    tf = {} ; current = None
+    with open (path,'r') as f:
+        for line in map(splice, f.readlines()):
+            if len(line) != 3:
+                continue
+            if line[0] == current:
+                tf[current][line[1]] = int(line[2])
+            else:
+                current = line[0]
+                tf[current] = { line[1] : int(line[2]) }
+    return tf
+
+
+def readIDF(path):
+    idf = {}
+    with open (path,'r') as f:
+        for line in map(splice, f.readlines()):
+            idf[line[0]] = float(line[1])
+    return idf
+
+
+def calcScore(D,Q,tfmap,idfmap,wcmap,k1,b):
+    total = 0
+    clean = re.sub(r'[^\s\w]+','',Q.lower())
+    for qi in clean.split(' '):
+        if qi not in tfmap[D]:
+            continue
+        tf = tfmap[D][qi]
+        idf = idfmap[qi]
+        wc = wcmap[D]
+        avg = sum(wcmap[f] for f in wcmap) / float(len(wcmap))
+        total += idf * tf * (k1+1) / (tf + k1 * (1 - b + b * wc / avg))
+    return total        
+
+
 if __name__ == '__main__':
-    D = sys.argv[1]
-    Q = sys.argv[2]
-    for qi in Q:
-        print qi
+    #D = sys.argv[1]
+    Q = sys.argv[1]
+    k1 = float(sys.argv[2])
+    b = float(sys.argv[3])
+
+    idfmap = readIDF(idfpath)
+    tfmap = readTF(tfpath)
+    wcmap = readWordCount(wordcountpath)
+    files = os.listdir(inputdir)
+    results = []
+    for f in files:
+        results += [(f,calcScore(f,Q,tfmap,idfmap,wcmap,k1,b))]
+    results = sorted(results, key=lambda tup: tup[1],reverse=True)
+    for result in results:
+        print result
