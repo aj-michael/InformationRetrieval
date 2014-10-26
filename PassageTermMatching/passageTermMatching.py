f0d84a4bd1a4e2866978bd4acd3861cb583a87b8
BenS5872 <bens5872@gmail.com>
2014-10-26 00:53:22 -0400

    Got passage term matching into a working(?) state

--- PassageTermMatching/passageTermMatching.py
+++ PassageTermMatching/passageTermMatching.py
@@ -6,34 +6,6 @@ import string
 import sys
 import math
 
-#input query from command prompt, as list of terms
-query = sys.argv[1]
-query = getTerms(query)
-
-inputdir = '../BM25Scoring/'
-
-#extract total number of documents from N.dat
-ninput = 'N.dat'
-with open (inputdir+ninput,'r') as f:
-    N = int(f.read())
-
-#extract term frequencies from dfcount.dat
-dfcount = {}
-with open (inputdir+dfcountinput,'r') as f:
-    for line in map(lambda x: x.strip().split('\t'), f.readlines()):
-        if len(line) == 2:
-            dfcount[line[0]] = int(line[1])
-
-#compute passage scores for each document
-passageScores = {}            
-for fname in os.listdir(input_dir):
-    passageScores[fname] = passageScore(fname)
-
-#print top 10 documents
-sortedKeys = sorted(passageScores, key=passageScores.get)
-for i in range(10):
-    print(sortedKeys[i])
-
 # returns the passage score for a given passage
 def passageScore(passage):
     numer = 0
@@ -45,23 +17,67 @@ def passageScore(passage):
             wij = idf(term)
         numer += wij
         denom += idf(term)
+        #print term, ':', wij, idf(term), numer, denom
 
     #return passage score
     return numer / denom
 
 # returns if the given passage contains the given file
 def fileContains(term,passage):
-    inputdir = '../Presidents'
-    with open(inputdir+passage,'r') as f:
-        return term in f
+    return term in containsLists[passage]
 
 # returns the idf of the given term
 def idf(term):
-    return math.log(N/(dfcount[term]+1))
+    c = 0
+    if term in dfcount.keys():
+        c = dfcount[term]
+    #print c, N, math.log(N/float(c+1))
+    return math.log(N/float(c+1))
 
 #converts input of string to list of terms
 def getTerms(terms):
-    outList = {}
+    outList = []
     for word in terms.split(' '):
-        outList.append(word)
+        outList.append(word.lower())
     return outList
+
+#input query from command prompt, as list of terms
+query = sys.argv[1]
+query = getTerms(query)
+
+inputdir = '../BM25Scoring/data/'
+
+#extract total number of documents from N.dat
+ninput = 'N.dat'
+with open (inputdir+ninput,'r') as f:
+    N = int(f.read())
+
+#extract term frequencies from dfcount.dat
+dfcount = {}
+dfcountinput = 'dfcount.dat'
+with open (inputdir+dfcountinput,'r') as f:
+    for line in map(lambda x: x.strip().split('\t'), f.readlines()):
+        if len(line) == 2:
+            dfcount[line[0]] = int(line[1])
+
+#extract contains list from dfcount.dat
+containsLists = {}
+tfcountinput = 'tfcount.dat'
+with open (inputdir+tfcountinput,'r') as f:
+    for line in map(lambda x: x.strip().split('\t'), f.readlines()):
+        if len(line) == 3:
+            if line[0] not in containsLists.keys():
+                containsLists[line[0]] = []
+            containsLists[line[0]].append(line[1])
+
+#compute passage scores for each document
+passageScores = {}
+inputdir = '../Presidents/'
+for fname in os.listdir(inputdir):
+    passageScores[fname] = passageScore(fname)
+
+#print top 10 documents
+sortedKeys = sorted(passageScores, key=passageScores.get,reverse=True)
+print(passageScores),'\n\n'
+for i in range(10):
+    print(sortedKeys[i])
